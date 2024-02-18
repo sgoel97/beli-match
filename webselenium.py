acaed0bb62b60f0747e835bd2cf55c01bd246c0f
Joyce-Yuan <joyceyuan80@gmail.com>
2024-02-17 17:26:25 -0800

    web scrapper outputs top restaurants

--- webselenium.py
+++ webselenium.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
+import json
 
 import os
 
@@ -38,15 +39,42 @@ soup = BeautifulSoup(page_source, 'html.parser')
 formatted_html = soup.prettify()
 
 # Parsing part: Find elements using BeautifulSoup
-print("trying to find elements")
-for element in soup.find_all('tag', {'class': 'text-item-title'}):  # Customize tag and class
-    print(element.text)  # or any other attribute or operation
+top_restaurants = {}
+
+for i, element in enumerate(soup.find_all('p', {'class': 'text-item-title'})):  # Customize tag and class
+    name = element.text.split('.')[1].strip()
+    print(name)
+    top_restaurants[i] = {'name': name}
+
+
+for i, element in enumerate(soup.find_all('p', {'class': 'text-item-subtitle'})):  # Customize tag and class
+    # even numbers are price and tags
+    if (i%2 == 0):
+        # dollar, tags, *_ = element.text.split('|')
+        array = element.text.split('|')
+        # print(array)
+        dollar = array[0].strip()
+        tags = []
+        if len(array) > 1:
+            tags = [tag.strip() for tag in array[1].split(",")]
+        top_restaurants[i//2]['price'] = dollar.strip()
+        top_restaurants[i//2]['tags'] = tags
+    else:
+        # odd numbers are locations
+        location = element.text.strip()
+        top_restaurants[i//2]['location'] = location
+    # top_tags[i] = element.text
+
+print(top_restaurants)
+# write the dictionary as a json file
+with open(current_dir + '/top_restaurants.json', 'w') as file:
+    json.dump(top_restaurants, file)
 
 # Define the filename for the prettified HTML content
-file_path = current_dir + '/formatted_webpage_source_will.txt'
+# file_path = current_dir + '/formatted_webpage_source_will.txt'
 
 # Save the formatted HTML to a file
-with open(file_path, 'w', encoding='utf-8') as file:
-    file.write(formatted_html)
+# with open(file_path, 'w', encoding='utf-8') as file:
+#     file.write(formatted_html)
 
-print(f"The formatted HTML has been saved as '{file_path}'.")
+# print(f"The formatted HTML has been saved as '{file_path}'.")
