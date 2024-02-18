3a73409a95f6f57ce02d32e5b426502b35b6fade
Timothy G <timg51237@gmail.com>
2024-02-17 18:07:50 -0800

    Multistage emails

--- server/scripts/send_emails.py
+++ server/scripts/send_emails.py
@@ -1,4 +1,46 @@
-import smtplib, ssl
+import smtplib, ssl, csv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def read_csv():
+    with open('your_file.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            print(row)
+
+def make_email(sender_email, receiver_email):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    Hi,
+    How are you?
+    Real Python has many great tutorials:
+    www.realpython.com"""
+    
+    html = """\
+    <html>
+    <body>
+        <p>Hi,<br>
+        How are you?<br>
+        <a href="http://www.realpython.com">Real Python</a> 
+        has many great tutorials.
+        </p>
+    </body>
+    </html>
+    """
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    message.attach(part1)
+    message.attach(part2)
+
+    return message.as_string()
 
 
 def send_email():
@@ -15,12 +57,9 @@ def send_email():
         # TODO: Send email here
         
         receiver = "sgoel9@berkeley.edu"
-        message = """\
-Subject: Hi there
 
-This message is sent from Python."""
-        server.sendmail("belimatch90@gmail.com", receiver, message)
-        server.sendmail("belimatch90@gmail.com", "timg51237@gmail.com", message)
+        server.sendmail("belimatch90@gmail.com", receiver, make_email("belimatch90@gmail.com", receiver))
+        server.sendmail("belimatch90@gmail.com", "timg51237@gmail.com", make_email("belimatch90@gmail.com", "timg51237@gmail.com"))
         print("Email sent!")
 
 
